4c14622eee281ca33fb44529bd344de5ae6f0701
pharmony01 <rileypeters2016@gmail.com>
2023-12-04 15:24:17 -0500

    Simulate move works, cost function works (but isnt great), minimax not functional

--- players/minimax.py
+++ players/minimax.py
@@ -3,7 +3,7 @@ import pdb
 import utils
 import random
 
-def get_computer_move(board, which_player):
+def get_computer_move(board: np.ndarray, which_player: int):
     """Search for the best move based on the current game state.
     Parameters
     ----------
@@ -17,14 +17,37 @@ def get_computer_move(board, which_player):
     choice : int
     The column (using 1-indexing!) that the player wants to drop a disc into.
     """
-    def is_terminal():
-        ...
-    
-    def minimax():
-        ...
-
-    def cost():
-        ...
-    
     valid_moves = utils.get_valid_moves(board)
+    pdb.set_trace()
+    best_move = minimax(board, which_player + 1, depth=3, alpha=float('-inf'), beta=float('inf'))
     return np.random.choice(valid_moves) + 1
+
+def minimax(board, player, depth, alpha, beta):
+    if depth == 0 or utils.is_gameover(board):
+        return
+    
+    for move in utils.get_valid_moves():
+        new_board = simulate_move(board, move, player)
+        value = cost(new_board, player)
+
+# It runs, not great heuristic though (fix later)
+def cost(board, player):
+    if utils.is_winner(board, player):
+        return 1e10 # Large number, might have to change this later
+    value = -1
+    # Check for multiple in sequence in rows
+    for row in board:
+        value = max(len(sum(np.where(row == player))), value)
+    # Check for multiple in sequence in cols
+    for col in range(board.shape[1] - 1):
+        value = max(len(sum(np.where(board[:,col] == player))), value)
+    return value
+
+# Works as intended
+def simulate_move(board, move, player):
+    new_board = board.copy()
+    new_board[np.argmin(board[:, move])][move] = player
+    return new_board
+
+
+    
